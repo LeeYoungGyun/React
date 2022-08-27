437e3525ce113500055e685d2d34417b590a6da7
YoungGyun <noxium@naver.com>
2022-08-27 18:34:52 +0900

    React

--- Moive App/src/App.js
+++ Moive App/src/App.js
@@ -1,18 +1,24 @@
 import React from "react";
 
-function Food( {favorite} ){
-    return <h1>I like {favorite}</h1>
-}
-
-const foodILIke = []
-
-function App() {
-  return (
-    <div>
-      <h1>Hello</h1>
-
-    </div>
-  );
+class App extends React.Component {
+    state = {
+        count: 0
+    };
+    add = () => {
+        this.setState(current => ({ count: current.count + 1 }));
+    }
+    minus = () => {
+        this.setState({ count: this.state.count -1 });
+    }
+    render() {
+        return(
+            <div>
+                <h1> The number is: {this.state.count}</h1>
+                <button onClick={this.add}>Add</button>
+                <button onClick={this.minus}>Minus</button>
+            </div>
+        );
+    }
 }
 
 export default App;
